9d9c1247bbd67d6771f052c38c10d5d4f7eccd98
ankit0695 <92664264+ankit0695@users.noreply.github.com>
2025-11-20 16:06:28 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,20 @@
+# take two number input from user and perform functions on it .
+
+num1=int(input("please enter first number "))
+num2=int(input("please enter second number "))
+
+#addition
+sum = num1+num2
+print("sum is :",sum)
+
+# subtraction
+subtract= num1 - num2
+print("subtraction is :",subtract)
+
+# multiplication
+mux = num1 * num2
+print("multiplication is :",mux)
+
+#division
+div= num1/num2
+print("division is :",round(div, 3))
---
+++ assignment 1_1.py
@@ -0,0 +1,6 @@
+# take input of first and second name from user and concatinate them
+
+f_name =str(input("enter first name "))
+l_name =str(input("enter last name "))
+fullname = f_name + " " + l_name
+print("hello " ,fullname,"welcome to python")
